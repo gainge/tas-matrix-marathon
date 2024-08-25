84a4cf5ccb34910c7c793582e0c4326c099d93da
Gibson Ainge <gainge@gmail.com>
2024-08-25 09:52:05 -0600

    update for peach stage

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('show me what you got');
 
 let intervalId;
 
-const WEEK_NUMBER = 4;
+const WEEK_NUMBER = 5;
 const THEME_WINDOW_END = 1722751199000 + (WEEK_NUMBER - 1) * (1000 * 60 * 60 * 24 * 7);
 
 const PLAYER_INDEX = 0;
@@ -93,8 +93,8 @@ const RUNS_DIR = 'data';
 const IMG_DIR = 'img';
 const STOCK_ICON_DIR = 'icons';
 
-const CURRENT_CHAR = CHARACTER_KEYS.Marth;
-const PREV_CHAR = CHARACTER_KEYS.Zelda;
+const CURRENT_CHAR = CHARACTER_KEYS.Peach;
+const PREV_CHAR = CHARACTER_KEYS.Marth;
 const STAGE_RUN_EXTENSION = '.csv'
 const STOCK_ICON_EXTENSION = '.png'
 
