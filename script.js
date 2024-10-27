0d0e6657f97ceeeb9599e50f7ba7532e900db02c
Gibson Ainge <gainge@gmail.com>
2024-10-27 11:05:54 -0600

    begin bowser week

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('show me what you got');
 
 let intervalId;
 
-const WEEK_NUMBER = 13;
+const WEEK_NUMBER = 14;
 const THEME_WINDOW_END = 1722751199000 + (WEEK_NUMBER - 1) * (1000 * 60 * 60 * 24 * 7);
 
 const PLAYER_INDEX = 0;
@@ -93,8 +93,8 @@ const RUNS_DIR = 'data';
 const IMG_DIR = 'img';
 const STOCK_ICON_DIR = 'icons';
 
-const CURRENT_CHAR = CHARACTER_KEYS.DK;
-const PREV_CHAR = CHARACTER_KEYS.Ness;
+const CURRENT_CHAR = CHARACTER_KEYS.Bowser;
+const PREV_CHAR = CHARACTER_KEYS.DK;
 const STAGE_RUN_EXTENSION = '.csv'
 const STOCK_ICON_EXTENSION = '.png'
 
@@ -111,6 +111,7 @@ const PAST_CHARACTERS = [
     CHARACTER_KEYS.Link,
     CHARACTER_KEYS.Luigi,
     CHARACTER_KEYS.Ness,
+    CHARACTER_KEYS.DK,
 ]
 
 function viewTotals() {
