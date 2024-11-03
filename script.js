e0cc39a9244d9aafcbc05c37c2e9bc0aa0c1c2eb
Gibson Ainge <gainge@gmail.com>
2024-11-03 08:05:47 -0700

    start fox week

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('show me what you got');
 
 let intervalId;
 
-const WEEK_NUMBER = 14;
+const WEEK_NUMBER = 15;
 const THEME_WINDOW_END = 1722751199000 + (WEEK_NUMBER - 1) * (1000 * 60 * 60 * 24 * 7);
 
 const PLAYER_INDEX = 0;
@@ -93,8 +93,8 @@ const RUNS_DIR = 'data';
 const IMG_DIR = 'img';
 const STOCK_ICON_DIR = 'icons';
 
-const CURRENT_CHAR = CHARACTER_KEYS.Bowser;
-const PREV_CHAR = CHARACTER_KEYS.DK;
+const CURRENT_CHAR = CHARACTER_KEYS.Fox;
+const PREV_CHAR = CHARACTER_KEYS.Bowser;
 const STAGE_RUN_EXTENSION = '.csv'
 const STOCK_ICON_EXTENSION = '.png'
 
@@ -112,6 +112,7 @@ const PAST_CHARACTERS = [
     CHARACTER_KEYS.Luigi,
     CHARACTER_KEYS.Ness,
     CHARACTER_KEYS.DK,
+    CHARACTER_KEYS.Bowser,
 ]
 
 function viewTotals() {
