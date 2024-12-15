66cd0822c692b10fdb4e032a27b064e61e9e9877
Gibson Ainge <gainge@gmail.com>
2024-12-15 09:30:33 -0700

    start ics week

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ console.log('show me what you got');
 
 let intervalId;
 
-const WEEK_NUMBER = 20;
+const WEEK_NUMBER = 21;
 const THEME_WINDOW_END = 1722751199000 + (WEEK_NUMBER - 1) * (1000 * 60 * 60 * 24 * 7);
 
 const PLAYER_INDEX = 0;
@@ -93,8 +93,8 @@ const RUNS_DIR = 'data';
 const IMG_DIR = 'img';
 const STOCK_ICON_DIR = 'icons';
 
-const CURRENT_CHAR = CHARACTER_KEYS.Pikachu;
-const PREV_CHAR = CHARACTER_KEYS.M2;
+const CURRENT_CHAR = CHARACTER_KEYS.ICs;
+const PREV_CHAR = CHARACTER_KEYS.Pikachu;
 const STAGE_RUN_EXTENSION = '.csv'
 const STOCK_ICON_EXTENSION = '.png'
 
@@ -118,6 +118,7 @@ const PAST_CHARACTERS = [
     CHARACTER_KEYS.Pichu,
     CHARACTER_KEYS.Falcon,
     CHARACTER_KEYS.M2,
+    CHARACTER_KEYS.Pikachu,
 ]
 
 function viewTotals() {
